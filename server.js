30d156b975b332a5d6d28e39ff5bac6710f8b3b4
Scott <macnomad84@gmail.com>
2016-09-14 16:51:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,21 +6,19 @@ var express = require('express'),
     server = http.createServer(app),
     io = io.listen(server);
 
-
-//socket routines
+//data objects
 
 var paddleOne = require('./game/paddle')("PlayerOne");
 var paddleTwo = require('./game/paddle')("PlayerTwo");
 var ball = require('./game/ball')(paddleOne,paddleTwo);
 
+//socket routines
 var base = require('./sockets/base')(io, ball,paddleOne,paddleTwo);
-
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.use(express.static('public'));
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
-
 
 server.listen(app.get('port'),app.get('ip'), function() {
 	console.log('Server instantiated at ' + Date.now());
