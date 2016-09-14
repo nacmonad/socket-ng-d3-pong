cdd8928eebf766b49065267c4e6ed8cdbbace2ac
nacmonad <macnomad84@gmail.com>
2016-09-14 16:49:56 -0400

    renamed server.js

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-var express = require ('express');
-var app = express();
-var path = require('path');
+var express = require('express'),
+    io = require('socket.io'),
+    http = require('http'),
+    app = express(),
+    path = require('path'),
+    server = http.createServer(app),
+    io = io.listen(server);
 
+
+//socket routines
+
+var paddleOne = require('./game/paddle')("PlayerOne");
+var paddleTwo = require('./game/paddle')("PlayerTwo");
+var ball = require('./game/ball')(paddleOne,paddleTwo);
+
+var base = require('./sockets/base')(io, ball,paddleOne,paddleTwo);
+
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.use(express.static('public'));
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
 
-app.get('/', function(req, res) {
-	res.sendFile('index.html');
-	console.log("sending from " + path)
-});
 
-app.listen(8080);	
+server.listen(app.get('port'),app.get('ip'), function() {
+	console.log('Server instantiated at ' + Date.now());
+    console.log('Listening @' + app.get('ip') + ' on port '+ app.get('port'));
+});
